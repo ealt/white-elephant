2a4fde18d4baa4ed242b6165f2a09fd284237ebc
Eric Alt <ericallenalt@gmail.com>
2022-01-01 01:17:37 -0600

    [refactor] change py_lib_path to py_pkgs_path
    The package directory seems to depend on the OS:
    - `dist-packages/` on Linux
    - `site-packages/` on macOS

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ import yaml
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
-py_lib_path = config['py_lib_path']
+py_pkgs_path = config['py_pkgs_path']
 
-we_module = Extension(
-    'white_elephant',
-    sources=[
-        'white_elephant.c',
-        'simulate.c',
-        'count_array.c',
-    ],
-    include_dirs=[f'{py_lib_path}/dist-packages/numpy/core/include/'])
+we_module = Extension('white_elephant',
+                      sources=[
+                          'white_elephant.c',
+                          'simulate.c',
+                          'count_array.c',
+                      ],
+                      include_dirs=[f'{py_pkgs_path}/numpy/core/include/'])
 
 setup(ext_modules=[we_module])
